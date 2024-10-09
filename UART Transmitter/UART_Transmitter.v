b71cd72ecc758327f62835b314059377b3a673e1
Abdul Karim Tamim <116204905+abdultamim02@users.noreply.github.com>
2024-10-08 19:21:45 -0700

    Update UART_Transmitter.v

--- UART Transmitter/UART_Transmitter.v
+++ UART Transmitter/UART_Transmitter.v
@@ -4,20 +4,12 @@
 // Engineer: Abdul Karim Tamim
 //////////////////////////////////////////////////////////////////////////////////
 
-module UART_Transmitter #(
-                          parameter N = 10,          // Total number of BITS transmitted 1: Start Bit, 8 Data Bits, 1 Optional Parity Bit (NOT USED), 1 Stop Bit --> 1 + 8 + 1 = 10 Bits
-                          parameter D = 8,           // Total number of Data bits
-                          parameter SB_TICK = 16     // Number of stop bit / oversampling cycles (1 stop bit)
 
-                         )
-                         (
-                          input clk,                 // System clock
-                          input reset,               // Reset signal
-                          input tx_start,            // Input signal to start the transmission process
-                          input [D-1:0] data,        // 8-bit data input
-                          input baud_clk,            // Baud clock signal from the Baud Rate Generator
-                          output reg tx_done,        // End of transmission signal
-                          output tx_out              // Serial data output for transmission (UART data line)
+module UART_Transmitter (input baud_clk,           // Generated baud clock from UART_Baud_Rate_Gen module
+                         input [7:0] data,         // 8-bit data to transmit
+                         input data_valid,         // Signal to indicate whether there is data to -> transmit <- or not (data_valid = 1: there is data to transmit | data_valid = 0; there is NOT data to transmit)
+                         output reg tx,            // Serial data output bit (1-bit)
+                         output reg tx_busy        // Signal to indicate whether transmission is -> busy <- or not (tx_busy = 1: transmitter is busy and currently involved in transmitting data | tx_done = 0: transmitter is not busy or has completed its transmission)
                          );
     
     // State Machine States
@@ -25,95 +17,54 @@ module UART_Transmitter #(
                      START = 2'b01,
                      DATA = 2'b10,
                      END = 2'b11;
-    
-    // Initialize Registers
-    reg [1:0] current_state, next_state;            // Initialize current state and next state registers
-    reg [3:0] baud_counter, next_baud_counter;      // Baud counter for timing
-    reg [2:0] bit_counter, next_bit_counter;        // Number of bits transmitted in data state
-    reg [N-1:0] data_reg, next_data_reg;            // Data word to transmit serially
-    reg tx_reg, next_tx_reg;                        // Data filter for potential glitches
-
-    // Register Logic
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            current_state <= IDLE;
-            baud_counter <= 0;
-            bit_counter <= 0;
-            data_reg <= 0;
-            tx_reg <= 1'b1;
-        end
-        else begin
-            current_state <= next_state;
-            baud_counter <= next_baud_counter;
-            bit_counter <= next_bit_counter;
-            data_reg <= next_data_reg;
-            tx_reg <= next_tx_reg;
-        end
-    end
-    
-    // State Machine Logic
-    always @* begin
-        next_state = current_state;
-        tx_done = 1'b0;
-        next_baud_counter = baud_counter;
-        next_bit_counter = bit_counter;
-        next_data_reg = data_reg;
-        next_tx_reg = tx_reg;
+                     
+    reg [1:0] state = IDLE;        // Current_state of the machine and next_state of the machine
+    reg [2:0] bit_index = 0;       // The index of the bit to be transmitted. Since we are 8-bis of data, this bit_index is capable of transmitting all 8 since it is 3-bits, which can range from 0 to 7
+    reg [7:0] tx_data;             // Data to be transmitted
         
-        case (current_state)
-            IDLE: begin                     // No data in FIFO
-                next_tx_reg = 1'b1;         // Transmit idle
-                if (tx_start) begin          // When FIFO is NOT empty
-                    next_state = START;
-                    next_baud_counter = 0;
-                    next_data_reg = data;
+    always @(baud_clk) begin
+        case (state)
+            IDLE: begin
+                tx <= 1;                // Logic HIGH (1) output bit (IDLE state)
+                tx_busy <= 0;               // Transmitter is not busy
+                // If data_valid signal is HIGH (equals to 1) meaning there is data to transmit
+                if (data_valid) begin
+                    state <= START;         // Move to START state
                 end
             end
             
             START: begin
-                next_tx_reg = 1'b0;          // Start bit
-                if (baud_clk) begin
-                    if (baud_counter == 15) begin
-                        next_state = DATA;
-                        next_baud_counter = 0;
-                        next_bit_counter = 0;
-                    end
-                    else
-                        next_baud_counter = baud_counter + 1;
-                end
+                tx <= 0;                // START bit always LOW
+                tx_busy <= 1;           // Transmitter is busy
+                tx_data <= data;        // Load data to tx_data
+                state <= DATA;          // Move to DATA state
             end
             
             DATA: begin
-                next_tx_reg = data_reg[0];  // Transmit current data bit
-                if (baud_clk) begin
-                    if (baud_counter == 15) begin
-                        next_baud_counter = 0;
-                        next_data_reg = data_reg >> 1;  // Shift right for next bit
-                        if (bit_counter == (N - 1))
-                            next_state = END;  // All data bits transmitted
-                        else
-                            next_bit_counter = bit_counter + 1;
-                    end
-                    else
-                        next_baud_counter = baud_counter + 1;
-                end
+                tx <= tx_data[bit_index];           // Transmit one bit at a time
+                // Increment bit index to transmit all 8 bits
+                if (bit_index < 7)
+                    bit_index = bit_index + 1;
+                else
+                    state = END;                // Move to END state
             end
             
             END: begin
-                next_tx_reg = 1'b1;          // Back to idle
-                if (baud_clk) begin
-                    if (baud_counter == (SB_TICK - 1)) begin
-                        next_state = IDLE;
-                        tx_done = 1'b1;      // Transmission complete
-                    end
-                    else
-                        next_baud_counter = baud_counter + 1;
-                end
+                tx <= 1;                 // STOP bit always HIGH
+                bit_index <= 0;          // Reset bit index back to 0
+                tx_busy <= 0;            // Transmitter is not busy
+                
+                // Check if there is more data to transmit
+                if (data_valid)
+                    state <= START;         // If there is, move to STATE state
+                else
+                    state <= IDLE;          // If there is not, move to IDLE state
             end
-        endcase    
+            
+            // Default state
+            default: state <= IDLE;
+            
+        endcase
     end
-    
-    // Output Logic
-    assign tx_out = tx_reg;
- 
+        
 endmodule
